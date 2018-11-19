fd1834f99813d7b344fce06288f34ac5e779b1e8
Paul Storey <Paul Storey>
2018-11-19 09:27:07 +0000

    Button click events and dynamic changes to CSS

--- src/main/java/org/iddo/gwt/tutorial/client/Tutorial.java
+++ src/main/java/org/iddo/gwt/tutorial/client/Tutorial.java
@@ -1,6 +1,9 @@
 package org.iddo.gwt.tutorial.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -13,7 +16,23 @@ public class Tutorial implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    HTML html = new HTML("<p>Welcome to GWT application</p>");
-    RootPanel.get("gwtContainer").add(html);
+      // add button to change font to big when clicked.
+      Button btn1 = new Button("Big Text");
+      btn1.addClickHandler(new ClickHandler() {
+          public void onClick(ClickEvent event) {
+              RootPanel.get("mytext").setStyleName("gwt-Big-Text");
+          }
+      });
+
+      // add button to change font to small when clicked.
+      Button Btn2 = new Button("Small Text");
+      Btn2.addClickHandler(new ClickHandler() {
+          public void onClick(ClickEvent event) {
+              RootPanel.get("mytext").setStyleName("gwt-Small-Text");
+          }
+      });
+
+      RootPanel.get("gwtGreenButton").add(btn1);
+      RootPanel.get("gwtRedButton").add(Btn2);
    }
 }
